a6704b2a1c09797447b958dd1b95a8b748fb124a
Jess Harrell <jharrell@pillartechnology.com>
2018-05-29 10:42:18 -0500

    add new projects for word-chain kata

---
+++ word-chain-test/ChainCreatorTest.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace word_chain_test
+{
+    public class ChainCreatorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ word-chain/ChainCreator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace word_chain
+{
+    public class ChainCreator
+    {
+    }
+}
